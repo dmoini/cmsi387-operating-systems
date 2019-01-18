f6103e55746e6938a81029208aa634477853b020
Serena Zafiris <szafiris@lion.lmu.edu>
2019-01-17 19:11:05 -0800

    SayHello

---
+++ Classwork/Classwork01/SayHello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char * argv[] ) {
+  char name[20];
+  printf( "Enter a name: " );
+  fgets( name, 20, stdin );
+  printf( "\n\n   Hello, %s", name );
+  return 0;
+}
