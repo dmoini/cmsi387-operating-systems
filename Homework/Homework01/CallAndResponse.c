bfc034c0d12ea74402dbbfcdd7c6ace06dd46406
Donovan Moini <donovanmoini@gmail.com>
2019-01-24 19:00:06 -0800

    Updated CallAndResponse.c

--- Homework/Homework01/CallAndResponse.c
+++ Homework/Homework01/CallAndResponse.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
     char name[20];
@@ -8,18 +10,18 @@ int main() {
     int heightInInches;
     char eyeColor[20];
     printf("Enter your name: ");
-    scanf("%s", name);
+    fgets(name, 20, stdin);
     printf("Enter your age: ");
     scanf("%d", &age);
     printf("Enter your class year: ");
     scanf("%d", &classYear);
     printf("Enter your hair color: ");
-    scanf("%s", hairColor);
+    fgets(hairColor, 20, stdin);
     printf("Enter your height in inches: ");
     scanf("%d", &heightInInches);
     printf("Enter your eye color: ");
-    scanf("%s", eyeColor);
-    printf("\nName: %s \nAge: %d \nClass year: %d \nHair color: %s \nHeight in inches: %d \nEye color: %s \n", 
+    fgets(eyeColor, 20, stdin);
+    printf("\nName: %sAge: %d \nClass year: %d \nHair color: %s \nHeight in inches: %d \nEye color: %s \n", 
             name, age, classYear, hairColor, heightInInches, eyeColor);
     return 0;
 }
