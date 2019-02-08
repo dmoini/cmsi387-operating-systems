62a2e8aa7f37c4cb96b3c11d541e3169aeb68dc1
ianlizzo <ianliz22@icloud.com>
2019-02-07 20:19:44 -0800

    almost done

--- Classwork/Classwork04/schedPractice.c
+++ Classwork/Classwork04/schedPractice.c
@@ -1,25 +1,37 @@
 #include <pthread.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 struct KeyValue {
-    char* key;
-    int value;
+  char *key;
+  int value;
 };
 
-int main(int argc, char *argv[]) {
-    int quantum = 40;
-    typedef struct KeyValue KeyValue;
-    KeyValue *kp[argc - 1];
-    
+int main(int argc, char **argv) {
+  int quantum = 40;
+  struct KeyValue arr[argc];
+  size_t n = sizeof(arr) / sizeof(arr[0]);
+  int counter = 0;
+  for (int i = 1; i < argc - 1; i += 2) {
+    arr[counter].key = argv[i];
+    arr[counter].value = atoi(argv[i + 1]);
+    // printf("%s : %d\n", arr[i].key, arr[i].value);
+    counter++;
+  }
 
-    for (int i = 1; i < argc; i += 2) {
-        struct KeyValue kv;
-        kv.key = argv[i];
-        kv.value = argv[i + 1];
+  printf("%zu\n", n);
+  int limit = n / 2;
+  while (limit > 0) {
+    for (int j = 0; j < argc - 1; j += 1) {
+      arr[j].value -= quantum;
+      if (arr[j].value - quantum <= 0) {
+        arr[j].value = 0;
+        limit--;
+      }
+      //   printf("%d", counter);
+      printf("%s : %i  : %i\n", arr[j].key, quantum, arr[j].value);
     }
-
-    printf("%s %s", argv[1], argv[2]);
-    // printf("%d", argc);
-    return 0;
+  }
+  return 0;
 }
