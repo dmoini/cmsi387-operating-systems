9f2d76e827c219d595b39a655ba5572b6807a0bb
Donovan Moini <donovanmoini@gmail.com>
2019-02-08 02:05:40 -0800

    Finished schedPractice.c

--- Classwork/Classwork04/schedPractice.c
+++ Classwork/Classwork04/schedPractice.c
@@ -4,34 +4,41 @@
 #include <unistd.h>
 
 struct KeyValue {
-  char *key;
-  int value;
+    char *key;
+    int value;
 };
 
-int main(int argc, char **argv) {
-  int quantum = 40;
-  struct KeyValue arr[argc];
-  size_t n = sizeof(arr) / sizeof(arr[0]);
-  int counter = 0;
-  for (int i = 1; i < argc - 1; i += 2) {
-    arr[counter].key = argv[i];
-    arr[counter].value = atoi(argv[i + 1]);
-    // printf("%s : %d\n", arr[i].key, arr[i].value);
-    counter++;
-  }
-
-  printf("%zu\n", n);
-  int limit = n / 2;
-  while (limit > 0) {
-    for (int j = 0; j < argc - 1; j += 1) {
-      arr[j].value -= quantum;
-      if (arr[j].value - quantum <= 0) {
-        arr[j].value = 0;
-        limit--;
-      }
-      //   printf("%d", counter);
-      printf("%s : %i  : %i\n", arr[j].key, quantum, arr[j].value);
+int printKVThreads(struct KeyValue kv[], int size) {
+    int limitSubtraction = 0;
+    for (int i = 0; i < size; i++) {
+        if (kv[i].value > 0 && kv[i].value <= 40) {
+            printf("%s:%i:done\n", kv[i].key, kv[i].value);
+            kv[i].value -= 40;
+            limitSubtraction++;
+        } else if (kv[i].value > 40) {
+            kv[i].value -= 40;
+            printf("%s:40:%i\n", kv[i].key, kv[i].value);
+        }
     }
-  }
-  return 0;
+    return limitSubtraction;
+}
+
+int main(int argc, char **argv) {
+    int quantum = 40;
+    int len = argc - 1;
+    struct KeyValue kvArr[len / 2];
+    int kvCounter = 0;
+    for (int i = 1; i < len; i += 2) {
+        kvArr[kvCounter].key = argv[i];
+        kvArr[kvCounter].value = atoi(argv[i + 1]);
+        kvCounter++;
+    }
+
+    printf("\nKEY -- thread:used:remaining\n\n");
+
+    int limit = len / 2;
+    while (limit > 0) {
+        limit -= printKVThreads(kvArr, len / 2);
+    }
+    return 0;
 }
