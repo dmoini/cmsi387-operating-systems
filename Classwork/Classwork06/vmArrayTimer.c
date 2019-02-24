3fb22a41c6c847f3895b320707ab5204b42aecee
Donovan Moini <donovanmoini@gmail.com>
2019-02-24 13:46:34 -0800

    Updated vmArrayTimer.c

--- Classwork/Classwork06/vmArrayTimer.c
+++ Classwork/Classwork06/vmArrayTimer.c
@@ -1,19 +1,30 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main(int argc, char** argv) {
-  int* array;
-  int arraySize = atoi(argv[1]);
-  array = malloc(sizeof(int) * arraySize);
-  int i;
-  srand(23);
-  for (i = 0; i < arraySize; i++) {
-    array[i] = rand() % 100;
-    if (i % 4096 == 0) {
-      printf("%d\n", array[i]);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Please input a single argument of the desired length of the array.");
+        return 0;
     }
-  }
-  free(array);
-  return 0;
+    int length = strtol(argv[1], NULL, 10);
+    if (length < 0) {
+        printf("Please input a positive interger.");
+        return 0;
+    }
+    int nums[length];
+    srand(time(0));
+    for (int i = 0; i < length; i++) {
+        nums[i] = (rand() % 100) + 1;
+    }
+    int ithElement = 57;
+    printf("Here is every 4096th element in the array:\n");
+    printf("------------------------------------------\n");
+    for (int i = 0; i < length; i++) {
+        int iPlusOne = i + 1;
+        if ((i + 1) % ithElement == 0) {
+            printf("%d\n", nums[i]);
+        }
+    }
+    return 0;
 }
