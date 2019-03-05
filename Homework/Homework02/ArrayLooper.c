07e27b7010040e18cbd0a6936e765b2b298a529d
Donovan Moini <donovanmoini@gmail.com>
2019-03-04 17:04:36 -0800

    Updated to calculate average access time

--- Homework/Homework02/ArrayLooper.c
+++ Homework/Homework02/ArrayLooper.c
@@ -3,14 +3,13 @@
 #include<time.h>
 
 int main(int argc, char* argv[]) {
-    clock_t begin = clock();
     if (1 == argc) {
         printf("Please enter one argument, the array size.\n");
         exit(0);
     }
     int length = atoi(argv[1]) ;
-    printf("Using array of %d elements.", length);
-    int* vmArray = (int* ) malloc(sizeof(int) * length);
+    printf("Using array of %d elements.\n", length);
+    int* vmArray = (int*) malloc(sizeof(int) * length);
     if (NULL == vmArray) {
         printf("Could not allocate array of %d size.\n", length);
         exit(-1);
@@ -18,13 +17,29 @@ int main(int argc, char* argv[]) {
     srand(time(0));
     for (int i = 0; i < length; i++) {
         vmArray[i] = (rand() % 100);
-        if (i % 4096 == 0) {
-            printf("Array element %d is %d\n", i, vmArray[i]);
-        }
     }
+
+    int averageTimesLength = (length / 4096) + 1;
+    double averageTimes[averageTimesLength];
+
+    for (int i = 0; i < length; i += 4096) {
+        clock_t begin = clock();
+        // printf("Array element %d is %d\n", i, vmArray[i]);
+        clock_t end = clock();
+        double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+        averageTimes[i / 4096] = time_spent;
+    }    
+
+    double sum = 0.0;
+    for (int i = 0; i < averageTimesLength; i++) {
+        // printf("Access time: %fs\n", averageTimes[i]);
+        sum += averageTimes[i];
+    }
+
+    double averageTime = sum / (double) averageTimesLength;
+    printf("Average Access time: %fs\n", averageTime);
+
     free(vmArray);
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Execution time: %fs\n", time_spent);
     return 0;
 }
