90d51b24f021a2fbd2735dc1129cf55a5891d51d
Donovan Moini <donovanmoini@gmail.com>
2019-04-01 16:10:00 -0700

    Added code for dining_philosophers.py

--- Homework/Homework03/dining_philosophers.py
+++ Homework/Homework03/dining_philosophers.py
@@ -1,16 +1,8 @@
 import threading
 import random
 import time
- 
-# Dining philosophers, 5 Phillies with 5 forks. Must have two forks to eat.
-#
-# Deadlock is avoided by never waiting for a fork while holding a fork (locked)
-# Procedure is to do block while waiting to get first fork, and a nonblocking
-# acquire of second fork.  If failed to get second fork, release first fork,
-# swap which fork is first and which is second and retry until getting both.
-#  
-# See discussion page note about 'live lock'.
- 
+
+# Code based from https://rosettacode.org/wiki/Dining_philosophers#Python
 class Philosopher(threading.Thread):
  
     running = True
@@ -23,8 +15,7 @@ class Philosopher(threading.Thread):
  
     def run(self):
         while(self.running):
-            #  Philosopher is thinking (but really is sleeping).
-            time.sleep(random.randint(3,13))
+            time.sleep(random.uniform(1, 10))
             print(f'{self.name} is hungry.')
             self.dine()
  
@@ -47,22 +38,15 @@ class Philosopher(threading.Thread):
         fork1.release()
  
     def dining(self):			
-        print(f'{self.name} starts eating')
-        time.sleep(random.uniform(1,10))
-        print(f'{self.name} finished eating and leaves to think.')
+        print(f'{self.name} started eating')
+        time.sleep(random.uniform(1, 10))
+        print(f'{self.name} finished eating and put down forks')
  
-def DiningPhilosophers():
+def dining_philosophers():
     names = ['Socrates', 'Aristotle', 'Kant', 'Mills', 'Singer']
-    forks = [threading.Lock() for i in range(5)]
- 
+    forks = [threading.Lock() for i in range(5)] 
     philosophers = [Philosopher(names[i], forks[i % 5], forks[(i + 1) % 5]) for i in range(5)]
- 
-    # random.seed(507129)
-    Philosopher.running = True
     for p in philosophers: 
         p.start()
-    time.sleep(100)
-    Philosopher.running = False
-    print ('Now we are finishing.')
  
-DiningPhilosophers()
+dining_philosophers()
