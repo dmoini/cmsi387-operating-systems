a364bc8d3032b00fc2e64076f872f21c4b090ba8
Donovan Moini <donovanmoini@gmail.com>
2019-04-03 01:07:18 -0700

    Minor updates to dining_philosophers.py

--- Homework/Homework03/dining_philosophers.py
+++ Homework/Homework03/dining_philosophers.py
@@ -2,26 +2,24 @@ import threading
 import random
 import time
 
-# Code based from https://rosettacode.org/wiki/Dining_philosophers#Python
+# Code based on https://rosettacode.org/wiki/Dining_philosophers#Python
 class Philosopher(threading.Thread):
- 
+    
     running = True
- 
     def __init__(self, name, left_fork, right_fork):
         threading.Thread.__init__(self)
         self.name = name
         self.left_fork = left_fork
         self.right_fork = right_fork
- 
+
     def run(self):
         while(self.running):
             time.sleep(random.uniform(1, 10))
-            print(f'{self.name} is hungry.')
+            print(f'{self.name} is hungry')
             self.dine()
- 
+
     def dine(self):
         fork1, fork2 = self.left_fork, self.right_fork
- 
         while self.running:
             fork1.acquire(True)
             locked = fork2.acquire(False)
@@ -32,21 +30,20 @@ class Philosopher(threading.Thread):
             fork1, fork2 = fork2, fork1
         else:
             return
- 
         self.dining()
-        fork2.release()
         fork1.release()
- 
+        fork2.release()
+
     def dining(self):			
         print(f'{self.name} started eating')
         time.sleep(random.uniform(1, 10))
-        print(f'{self.name} finished eating and put down forks')
- 
+        print(f'{self.name} finished eating and put down his forks')
+
 def dining_philosophers():
     names = ['Socrates', 'Aristotle', 'Kant', 'Mills', 'Singer']
     forks = [threading.Lock() for i in range(5)] 
-    philosophers = [Philosopher(names[i], forks[i % 5], forks[(i + 1) % 5]) for i in range(5)]
+    philosophers = [Philosopher(names[i], forks[i], forks[(i + 1) % 5]) for i in range(5)]
     for p in philosophers: 
         p.start()
- 
+
 dining_philosophers()
