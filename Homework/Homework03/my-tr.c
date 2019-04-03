25bd320bddf2e04cec032dd89326773b5e8943bd
Donovan Moini <donovanmoini@gmail.com>
2019-04-02 19:15:52 -0700

    WIP

--- Homework/Homework03/my-tr.c
+++ Homework/Homework03/my-tr.c
@@ -1,32 +1,38 @@
 #include <unistd.h>
-#include <fcntl.h>
-// #include <sys/stat.h>
 #include <stdio.h> 
-#include <sys/types.h> 
+#include <fcntl.h>
+#include <sys/stat.h> 
 #include <unistd.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
+
+// issues: replacing characters with t and spaces with e
 
 int main(){
     pid_t returnedValue = fork();
-    if(returnedValue < 0){
+    // printf("returnedValue: %d\n", returnedValue);
+    if (returnedValue < 0) {
         perror("error forking");
         return -1;
-    } else if (returnedValue == 0){
-        if(close(STDOUT_FILENO) < 0){
-            perror("error closing standard output");
+    } else if (returnedValue == 0) {
+        printf("returnedValue == 0");
+        if (close(STDIN_FILENO) < 0) {  // STDOUT_FILENO
+            perror("error closing standard input");
             return -1;
-    }
-        if(open("my-processes", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR) < 0){
+        }
+        if (open("test.txt", O_RDONLY, S_IRUSR) < 0) {
             perror("error opening my-processes");
             return -1;
         }
-        execlp("tr", "tr", NULL, NULL);
+        printf("Before execlp");
+        execlp("tr", "tr", "a-z A-Z", "test.txt", NULL);
         perror("error executing tr");
         return -1;
-} else {
-        if(waitpid(returnedValue, 0, 0) < 0){
+    } else {
+        if (waitpid(returnedValue, 0, 0) < 0) {
             perror("error waiting for child");
             return -1;
         }
-        printf( "Note the parent still has the old standard output." );
+        printf( "Note the parent still has the old standard output.\n" );
     }
 }
