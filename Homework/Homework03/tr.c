384d68bcbe9361478f1bcabb588bd78de9d9a1ed
Donovan Moini <donovanmoini@gmail.com>
2019-04-02 20:00:47 -0700

    Renamed my-tr.c to tr.c

--- Homework/Homework03/tr.c
+++ Homework/Homework03/tr.c
@@ -6,38 +6,26 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-// issues: replacing characters with t and spaces with e
-
-int main()
-{
+int main() {
     pid_t returnedValue = fork();
-    // printf("returnedValue: %d\n", returnedValue);
-    if (returnedValue < 0)
-    {
+    if (returnedValue < 0) {
         perror("error forking");
         return -1;
-    }
-    else if (returnedValue == 0)
-    {
+    } else if (returnedValue == 0) {
         printf("returnedValue == 0");
-        if (close(STDIN_FILENO) < 0)
-        { // STDOUT_FILENO
+        if (close(STDIN_FILENO) < 0) { 
             perror("error closing standard input");
             return -1;
         }
-        if (open("test.txt", O_RDONLY, S_IRUSR) < 0)
-        {
+        if (open("/etc/passwd", O_RDONLY, S_IRUSR) < 0) {
             perror("error opening my-processes");
             return -1;
         }
         execlp("tr", "tr", "a-z", "A-Z", NULL);
         perror("error executing tr");
         return -1;
-    }
-    else
-    {
-        if (waitpid(returnedValue, 0, 0) < 0)
-        {
+    } else {
+        if (waitpid(returnedValue, 0, 0) < 0) {
             perror("error waiting for child");
             return -1;
         }
