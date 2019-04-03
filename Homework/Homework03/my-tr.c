f0c3614f625f762261ca9eb1ce1b598385a30034
Serena Zafiris <szafiris@lion.lmu.edu>
2019-04-02 18:31:41 -0700

    wip

--- Homework/Homework03/my-tr.c
+++ Homework/Homework03/my-tr.c
@@ -1,7 +1,9 @@
 #include <unistd.h>
-#include <stdio.h>
 #include <fcntl.h>
-#include <sys/stat.h>
+// #include <sys/stat.h>
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
 
 int main(){
     pid_t returnedValue = fork();
